fda1da035179e53009ef5406c9060a11809ebb64
amtepradnya <pradnyaahire4@gmnail.com>
2024-10-09 16:55:43 +0530

    testing apis

--- src/app.js
+++ src/app.js
@@ -1,9 +1,35 @@
 const express=require("express");
 const app=express();
 
-app.use("/hello",(req,res)=>{
+
+
+// app.use("/user",(req,res)=>{
+//     res.send("hii hello")
+// })
+// app.use("/test",(req,res)=>{
+//     res.send("hii test")
+// })
+
+app.get("/user",(req,res)=>{
+    res.send({"firstname":"prad","lastname":"amte"})
+
+    // console.log({"firstname":"prad","lastname":"amte"})
+})
+app.post("/user",(req,res)=>{
+    res.send({"firstname":"akshay","lastname":"amte"})
+
+    // console.log({"firstname":"prad","lastname":"amte"})
+})
+app.delete("/user",(req,res)=>{
+    res.send("delete call")
+
+    // console.log({"firstname":"prad","lastname":"amte"})
+})
+
+app.use("/",(req,res)=>{
     res.send("hii pradnya")
 })
+
 app.listen(3000,()=>{
 
     console.log("successfully started server")
